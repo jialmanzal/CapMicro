a85e6127931af42eec6d8cc5d243387ae220f10b
Jesús Ignacio Almanza León (Líder Técnico) <jialmanza@colsanitas.com>
2020-10-30 07:33:14 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, Response, json
+from database.db import initialize_db
+from resources.routes import  inicializar_rutas
+from flask_restful import Api
+from configparser import ConfigParser
+
+app = Flask(__name__)
+api = Api(app)
+
+config = ConfigParser()
+config.read('config.properties')
+
+app.config['CONFIGURACION_MONGO'] = {
+    'host': config.get(section='database_config', option='host')
+    + config.get(section='database_config', option='database')
+}
+
+@app.route('/')
+def base():
+    return Response(response=json.dumps({
+        "Estado": "OK",
+        "base_datos": config.get(section='database_config', option='host')
+    }),status=200,
+    mimetype='application/json')
+
+initialize_db(app)
+inicializar_rutas(api)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, host='0.0.0.0')
